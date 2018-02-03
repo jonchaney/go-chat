ff6d989e1161714d620a616ef582bb39e41754ac
Jonathan Chaney <jonchaney@gmail.com>
2018-02-03 00:14:26 -0800

    websocket refactor--split into different files

--- main.go
+++ main.go
@@ -7,18 +7,6 @@ import (
 	"os"
 )
 
-// // global variables, usually bad style
-// var clients = make(map[*websocket.Conn]bool) // connected clients
-// var broadcast = make(chan Message)           // broadcast channel
-// var upgrader = websocket.Upgrader{}          // configure the upgrader
-
-// // Message object defined here
-// type Message struct {
-// 	// Email    string `json:"email"`
-// 	Username string `json:"username"`
-// 	Message  string `json:"message"`
-// }
-
 func main() {
 	// create simple file server
 	// in order to serve the index.html file the folder must be called static
@@ -62,52 +50,4 @@ func main() {
 
 // 	fmt.Println("Serving on port", port)
 // 	log.Fatal(http.ListenAndServe(port, router))
-// }
-
-// handles incoming WebSocket connections
-// func handleConnections(w http.ResponseWriter, r *http.Request) {
-// 	// upgrade intial GET request to a WebSocket
-// 	ws, err := upgrader.Upgrade(w, r, nil)
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-// 	defer ws.Close()
-
-// 	// register our new client by adding it to the global "clients" map
-// 	clients[ws] = true
-// 	// an infinite loop that listens for a new message to be written to the WebSocket
-// 	// unserializes it from JSON to a Message object and then puts it into the broadcast channel
-// 	for {
-// 		var msg Message
-// 		// read in new message as JSON and map it to a Message object
-// 		err := ws.ReadJSON(&msg)
-// 		if err != nil {
-// 			log.Printf("error: %v", err)
-// 			// we are assuming that if there is an error, the client is no longer connected,
-// 			// therefore we remove them from the clients map and exit the loops
-// 			delete(clients, ws)
-// 			break
-// 		}
-// 		// send the newly received message to the broadcast channel unless it is a manual ping
-// 		// refactor to sending ping packet to server with a pong response...?
-// 		if msg.Username != "ping" {
-// 			broadcast <- msg
-// 		}
-// 	}
-// }
-
-// // loop that continously reads from the 'broadcast' channel and then relays the message
-// // to all the the clients over their WebSocket connection
-// func handleMessages() {
-// 	for {
-// 		msg := <-broadcast
-// 		for client := range clients {
-// 			err := client.WriteJSON(msg)
-// 			if err != nil {
-// 				log.Printf("error: %v", err)
-// 				client.Close()
-// 				delete(clients, client)
-// 			}
-// 		}
-// 	}
 // }
